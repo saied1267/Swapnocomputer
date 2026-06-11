1212380181e34a201ddd3a6dd883cb2b69551688
saied1267 <154495703+saied1267@users.noreply.github.com>
2026-06-11 23:04:09 +0600

    Add files via upload

---
+++ generate_students.ts
@@ -0,0 +1,74 @@
+const data = [
+  // Image 1: Roll 141-154
+  ["Md Rayhan", "Md Lokman", "MMomena Begum", "Aman Bazar", "01325871562", "Computer Office Application", "141", "1234"],
+  ["Abu Obayda", "MD Nasir", "Saheda sultana", "Fokir para", "01882679001", "Computer Office Application", "142", "1234"],
+  ["Touhidul Islam Abir", "Abu Taher", "Nasima Akter", "Asraf alir Bari", "01870406514", "Computer Office Application", "143", "1234"],
+  ["Sayed khan rahat", "Shamsul alom", "Josna akter", "Jugirhat", "018xxxxxxxx", "Computer Office Application", "144", "1234"],
+  ["Hosen Al Jaber", "Jakir Hose", "Nilu Akter", "Jugirhat", "01863350746", "Computer Office Application", "145", "1234"],
+  ["Mahfuj Ahmed", "Monir Ahamed", "Sharmin akter", "Aman Bazar", "01896093213", "Computer Office Application", "146", "1234"],
+  ["Md arafat", "Mohammad Sarowar", "Asma begum", "SIKABPUR", "0181912236", "Computer Office Application", "147", "1234"],
+  ["Jinuh Marma", "#", "#", "Borodigirpar", "01619755108", "Computer Office Application", "148", "1234"],
+  ["Md shakil", "Abdus salam", "Mst Shanaj", "Cantonment", "01805058414", "Computer Office Application", "149", "1234"],
+  ["Sanjida Nur jerin", "Muhammad Nur Nobi", "Ruzi Akther", "Zugirhat", "01793044702", "Computer Office Application", "150", "1234"],
+  ["Nusrat Jahan", "Asraf Hosen", "Farhana Kanom", "Aman Bazar", "01617261259", "Computer Office Application", "151", "1234"],
+  ["Kakoli Das", "Mintu Das", "Purnima Das", "Aman Bazar", "01834792118", "Computer Office Application", "152", "1234"],
+  ["Asaduzzaman Rifat", "Ayub khan", "Ferdus Begum", "Aman bazar", "01861857505", "Computer Office Application", "153", "1234"],
+  ["anjan dev nath", "Sapan kumar Dev nath", "Nrva rani debi", "Aman bazar", "01861141277", "Computer Office Application", "154", "1234"],
+
+  // Image 2: Roll 101-119
+  ["Md Minhajul Kabir", "Humayoun Kabir", "Sarbin Akter", "Aman bazar", "01752563250", "Computer Office Application", "101", "2009"],
+  ["Nur Islam Babo", "Nasir Miya", "Sahin Akter", "Volioya para", "01886515246", "Computer Office Application", "102", "2007"],
+  ["Hamida Sulthana", "MD.Nasir", "Kulsuma Aktar", "Lalirhat", "01830346676", "Computer Office Application", "103", "2010"],
+  ["RAKIBUL Hassan", "Belal hossain", "Rahima akter", "Aman bazar", "01872676210", "Computer Office Application", "104", "2008"],
+  ["Suprity Shil", "Rakhal Shil", "Ratna Shil", "Khandakia", "01996725194", "Computer Office Application", "105", "2006"],
+  ["Tusha chowdhury", "Ajoy chowdhury", "Arssna chowdhury", "shikarpur", "01585727018", "Computer Office Application", "106", "2020"],
+  ["Jubayer Ivna", "Monsur Alam", "Rubi Akter", "Kulgaw", "01860260834", "Computer Office Application", "107", "1234"],
+  ["Shirin Akter", "Jane Alam", "Parvin Akter", "Kulgaw", "01727718747", "Computer Office Application", "108", "1234"],
+  ["Nusrat Alam", "Mohammed Khursed Alam", "Shamim Akter", "Khandakia", "01885340870", "Computer Office Application", "109", "1885"],
+  ["Emon", "Shahjhan", "Feroza", "Aman bazar", "01642870994", "Computer Office Application", "110", "1100"],
+  ["Sharmin Jerin", "Naser Uddin", "Jesmin Akter", "Notunpara", "01863374176", "Computer Office Application", "112", "7417"],
+  ["Hasan Tareq", "Mohammad syed hossaIN", "Rashada Akter", "Foteyabad", "1994446992", "Computer Office Application", "113", "1234"],
+  ["Taohid Tanvir", "Quabi Farid Hyder", "Nasira hyder", "Aman Bazar", "1812373946", "Computer Office Application", "114", "1234"],
+  ["Yasar Bin Faiz", "Faizul Islam Sikder", "Jesmin Akter", "aman bazar", "01631829762", "Computer Office Application", "115", "1234"],
+  ["Ikrar Jahan", "Md yousop", "taslima akter", "Aman bazar", "01821305317", "Computer Office Application", "116", "1234"],
+  ["Md minhaj ahamed fahim", "mintu", "rehena begum", "Aman bazar", "01824560010", "Computer Office Application", "117", "1234"],
+  ["ismot jerin nikita", "abul bashaR", "LUTFOR NAHAR", "Aman bazar", "01830218417", "Computer Office Application", "118", "1234"],
+  ["Meherunnesa joya", "mohammad jamir uddin", "khadija yeasmin", "Aman bazar", "01311813579", "Computer Office Application", "119", "1234"],
+
+  // Image 3: Roll 120-140
+  ["Rehena Akter Ranu", "Md. Osman Goni", "Nasima Akter", "Vuliya para", "01877918648", "Computer Office Application", "120", "1234"],
+  ["Sapana newaz tahiba", "Habib Ullah", "Jaheda Akter", "Notun para", "01877419244", "Computer Office Application", "121", "1234"],
+  ["Bibi Fatema happy", "Md idrish", "Bibi Khadiza", "Foteyabad", "01894108307", "Computer Office Application", "122", "1234"],
+  ["Tasnim Akter", "Bahadur Alam", "Yasmin Akter", "Foteyabad", "01617788376", "Computer Office Application", "123", "1234"],
+  ["Noman Uddin Tareq", "Abul Bashar", "Rosha Akter", "Shikarpur", "01881126795", "Computer Office Application", "124", "1234"],
+  ["Md Nayem Uddin", "Md Nizam Uddin", "Jesia Sharif Dilu", "Aman bazar", "01882694138", "Computer Office Application", "125", "1234"],
+  ["Papon Pal", "Ashutos Pal", "Dilu Pal", "Shikarpur", "01861210597", "Computer Office Application", "126", "1234"],
+  ["Mohammad Emon", "Md Farid Meah", "Parvin Akther", "Shikarpur", "01601034628", "Computer Office Application", "127", "1234"],
+  ["Sayed Hossen Fahim", "Mohammad Hossain", "Shahanaz Begum", "Sub register Bari", "01806255435", "Computer Office Application", "128", "1234"],
+  ["Farhan Islam", "Md Kamal Hossain", "Razia Sultana", "Khosal Shah", "01771010201", "Computer Office Application", "129", "1234"],
+  ["Md Jaber Jonaed", "Md Abdus Salam", "Shamsur nahar", "Nosrullah Kazi Bari", "01815478891", "Computer Office Application", "130", "1234"],
+  ["Irfanul Haq", "Anamul Haq", "Lucky Akter", "Vuliya Para", "01615749042", "Computer Office Application", "131", "1234"],
+  ["Shakib Hasan Hridoy", "Md Anwar Hossain", "Ms Halima Begum", "Cantonment", "01304330300", "Computer Office Application", "132", "1234"],
+  ["Hasibul Islam Ontor", "Md Anwar Hossain", "Ms Halima Begum", "Cantonment", "01318798163", "Computer Office Application", "133", "1234"],
+  ["Harunur Rashid", "Md Abdul Mannan", "NurnaharBegum", "Aman Bazar", "01836553630", "Computer Office Application", "134", "1234"],
+  ["Shahed ul Islam", "Jamal Uddin", "Roma Akter", "Abdul roshid Bari", "01941942704", "Computer Office Application", "135", "1234"],
+  ["Saidul islam Afif", "Md Rafique", "Rehana Akter", "Zugirhat", "01710845370", "Computer Office Application", "136", "1234"],
+  ["Ajmain Khan", "Saifuffin Khan", "Muna Khanam", "Aman Bazar", "01829977777", "Computer Office Application", "137", "1234"],
+  ["Tanzibul Islam", "Zahedul islam", "Jannatul Naiyen", "Aman Bazar", "01754025417", "Computer Office Application", "138", "1234"],
+  ["Mohammad Jayedul Hasan", "Md jalal uddin", "Shanaj Begum", "Juhgir Hat", "01828146802", "Computer Office Application", "139", "1234"],
+  ["Onik Mutsuddi", "Monojit Mutsuddi", "Purnima Mutsuddi", "Aman Bazar", "01610235434", "Computer Office Application", "140", "1234"]
+];
+
+const formatted = data.map(d => ({
+  roll: d[6],
+  name: d[0],
+  fatherName: d[1],
+  motherName: d[2],
+  address: d[3],
+  mobile: d[4],
+  course: d[5],
+  pin: d[7],
+  regDate: "2026-06-10"
+}));
+
+console.log(JSON.stringify(formatted, null, 2));
